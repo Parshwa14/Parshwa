3c91ba1daf411659b739a8056181e30ac73d76ff
Parshwa Bhavsar <bhavsarparshwa@gmail.com>
2022-11-24 11:20:39 +0530

    Practice

---
+++ C Programming/Classwork/24-11-2022/Practice1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+{
+	printf("\t\t\tWelcome to C Programming at Tops Technologies.\n");
+	printf("\nTime : 10.30 to 12:00");
+	printf("\nStudent Info");
+	printf("\nFirst Name : Parshwa");
+	printf("\nLast Name : Bhavsar");
+	printf("\nEmail Id : bhavsarparshwa@gmail.com");
+	printf("\nMobile No : 8511153980");
+	printf("\nCourse : Software Engineering");
+	printf("\nCourse Content : C, C++, HTML, Database, Python");
+	printf("\nCourse Duration : 6 Months");
+}
