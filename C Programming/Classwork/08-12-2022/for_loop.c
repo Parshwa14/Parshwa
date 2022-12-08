202dace02a249d537c93708ab2bb45a3e78f49ea
Parshwa Bhavsar <bhavsarparshwa@gmail.com>
2022-12-08 12:29:02 +0530

    c

---
+++ C Programming/Classwork/08-12-2022/for_loop.c
@@ -0,0 +1,22 @@
+/* for loop : all in one line
+	syntex :
+			
+			for(initialization;condition,updation)
+			{
+				statement;
+			}
+		*/
+
+
+
+#include <stdio.h>
+
+void main()
+{
+	int i;
+	
+	for(i=1;i<=10;i++)
+	{
+		printf("\n%d",i);
+	}
+}
