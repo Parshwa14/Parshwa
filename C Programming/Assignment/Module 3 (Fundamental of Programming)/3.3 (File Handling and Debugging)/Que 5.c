569c829d91562a32827533d38c22916dc5ac7040
Parshwa Bhavsar <bhavsarparshwa@gmail.com>
2022-12-14 18:23:56 +0530

    c

--- C Programming/Assignment/Module 3 (Fundamental of Programming)/3.3 (File Handling and Debugging)/Que 5.c
+++ C Programming/Assignment/Module 3 (Fundamental of Programming)/3.3 (File Handling and Debugging)/Que 5.c
@@ -1,1 +1,158 @@
-//Que 5 : WAP to take two Array input from user and sort them in ascending or descending order as per user’s choice.
+//Que 5 : Write a program to take two Array input from user and sort them in ascending or descending order as per user’s choice.
+
+#include <stdio.h>
+
+void main()
+{
+	int arr1 [5], arr2 [6], sort;
+	int i,j,temp;
+
+	
+//	Taking input from the user for array 1 elements
+
+
+	printf("\nFor Array 1 :\n\n");
+	for(i=0;i<6;i++)
+	{
+		printf("Enter element %d : ",i);
+		scanf("%d",&arr1[i]);	
+	}
+
+
+//	Taking input from the user for array 2 elements
+
+	
+	printf("\nFor Array 2 :\n\n");
+	for(i=0;i<7;i++)
+	{
+		printf("Enter element %d : ",i);
+		scanf("%d",&arr2[i]);
+	}
+
+
+//Showing the elements of array 1
+	
+	printf("\nThe elements of Array 1 : \n\n");
+	for(i=0;i<6;i++)
+	{
+		printf("%d\t",arr1[i]);
+	}
+
+
+//Showing the elements of array 2
+
+	
+	printf("\n\nThe elements of Array 2 : \n\n");
+	for(i=0;i<7;i++)
+	{
+		printf("%d\t",arr2[i]);
+	}
+
+
+//Making user chose to select how to sort ascending or descending	order
+
+
+	printf("\n\nEnter your choice to sort the array.");
+	printf("\n1 - Ascending order. \n2 - Descending order.");
+	scanf("%d",&sort);
+
+	if(sort==1)
+	{
+		//for array 1
+		
+		for(i=0;i<6;i++)
+		{
+			for(j=i+1;j<6;j++)
+			{
+				if(arr1[i]>arr1[j])
+				{
+					temp = arr1[i];
+					arr1[i]=arr1[j];					//logic for sorting
+					arr1[j]=temp;
+				}
+			}
+			
+		}
+		
+		printf("\nArray 1 in Ascending order : \n\n");
+		for(i=0;i<6;i++)
+		{																		
+			printf("%d\t",arr1[i]);								//showing the sorted array 1
+		}
+		
+//		for array 2
+
+		for(i=0;i<7;i++)
+		{
+			for(j=i+1;j<7;j++)
+			{
+				if(arr2[i]>arr2[j])
+				{
+					temp = arr2[i];						//logic for sorting
+					arr2[i]=arr2[j];
+					arr2[j]=temp;
+				}
+			}
+		}
+		printf("\nArray 2 in Ascending order : \n\n");
+		for(i=0;i<7;i++)
+		{					
+			printf("%d\t",arr2[i]);											//showing the sorted array 2
+		}
+	}	
+	
+	
+//Sorting in descending order
+
+
+	else if(sort==2)
+	{
+		
+		//for array 1
+		
+		for(i=0;i<6;i++)
+		{
+			for(j=i+1;j<6;j++)
+			{
+				if(arr1[i]<arr1[j])
+				{
+					temp = arr1[i];
+					arr1[i]=arr1[j];									//logic for sorting
+					arr1[j]=temp;
+				}			
+			}
+		}
+		
+		printf("\nArray 1 in Descending order : \n\n");
+		for(i=0;i<6;i++)
+		{		 		
+			printf("%d\t",arr1[i]);										//showing the sorted array 1
+		}
+		
+		
+		// for array 2 
+		for(i=0;i<7;i++)
+		{
+			for(j=i+1;j<7;j++)
+			{
+				if(arr2[i]<arr2[j])
+				{
+					temp = arr2[i];
+					arr2[i]=arr2[j];									//logic for sorting
+					arr2[j]=temp;
+				}
+			}
+		}
+
+		printf("\nArray 2 in Descending order : \n\n");
+		for(i=0;i<7;i++)
+		{
+			printf("%d\t",arr2[i]);										//showing the sorted array 2
+		}
+	}
+	else
+	{
+		printf("\nPlease select valid option.");
+	}
+
+}
--- C Programming/Assignment/Module 3 (Fundamental of Programming)/3.3 (File Handling and Debugging)/Que 6.c
+++ C Programming/Assignment/Module 3 (Fundamental of Programming)/3.3 (File Handling and Debugging)/Que 6.c
@@ -1,1 +1,109 @@
-//Que 6 : WAP to make addition, Subtraction and multiplication of two matrix using 2-D Array.
+//Que 6 : Write a program to make addition, Subtraction and multiplication of two matrix using 2-D Array.
+
+#include <stdio.h>
+
+void main()
+{
+	int mat1[3][3],mat2[3][3];
+	int i,j,op;
+	
+	//Taking the inputs of both the matrixs from user
+	
+	//matrix 1 
+	
+	printf("\nEnter the Elements for Matrix 1.");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("\nEnter value for element [%d][%d] ",i,j);
+			scanf("%d",&mat1[i][j]);
+		}
+	}
+	
+	
+	//matrix 2
+	
+	printf("\nEnter the Elements for Matrix 2.");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("\nEnter value for element [%d][%d] ",i,j);
+			scanf("%d",&mat2[i][j]);
+		}
+	}
+	
+	//showing the elements of matrix 1 and matrix 2 entered by user
+	
+	printf("\nMatrix 1 : \n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d\t",mat1[i][j]);
+		}
+		printf("\n");
+	}
+	
+	printf("\nMatrix 2 : \n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d\t",mat2[i][j]);
+		}
+		printf("\n");
+	}
+	
+	//For mathermatical operations
+	
+	printf("\nSelect an Operation from below.");																//user have to chose any of one operation
+	printf("\n1 for Addition. \n2 for Subtraction. \n3 for Multiplication.\n");
+	scanf("%d",&op);
+	
+	if(op==1)
+	{
+		printf("\nAddition of two matrix : \n\n");
+		for(i=0;i<3;i++)
+		{
+			for(j=0;j<3;j++)													//for the addition 
+			{
+				printf("%d\t",(mat1[i][j]+mat2[i][j]));
+			}
+			printf("\n");
+		}
+	}
+	
+	else if(op==2)
+	{
+		printf("\nSubtraction of two matrix : \n\n");
+		for(i=0;i<3;i++)
+		{
+			for(j=0;j<3;j++)													//for subtraction
+			{
+				printf("%d\t",(mat1[i][j]-mat2[i][j]));
+			}
+			printf("\n");
+		}
+	}
+	
+	else if(op==3)
+	{
+		printf("\nMultiplication of two matrix : \n\n");
+		for(i=0;i<3;i++)
+		{
+			for(j=0;j<3;j++)													//for multiplication
+			{
+				printf("%d\t",(mat1[i][j]*mat2[i][j]));
+			}
+			printf("\n");
+		}
+	}
+	
+	else
+	{
+		printf("\nError,Try again!");
+	}
+}
+
