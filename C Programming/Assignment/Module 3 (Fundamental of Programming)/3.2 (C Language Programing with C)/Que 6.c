b932ea95ae944141e42cecb642fcfd78e1397789
Parshwa Bhavsar <bhavsarparshwa@gmail.com>
2022-12-15 14:10:37 +0530

    c

--- C Programming/Assignment/Module 3 (Fundamental of Programming)/3.2 (C Language Programing with C)/Que 6.c
+++ C Programming/Assignment/Module 3 (Fundamental of Programming)/3.2 (C Language Programing with C)/Que 6.c
@@ -1,1 +1,19 @@
 //Que 6 Write a program to print factorial of given number.
+
+#include <stdio.h>
+
+void main()
+{
+	int n,i,f = 1;
+	
+	printf("\nEnter a Number to Obtain it's factorial : ");
+	scanf("%d",&n);
+	
+	for(i=1;i<=n;i++)
+	{
+		f = f * i;
+	}
+	printf("\n________________________________");
+	
+	printf("\n\nThe factorial of %d = %d",n,f);
+}
