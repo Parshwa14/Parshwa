ba207bc516bac41c288fbd38c59dae1d2ff752ba
Parshwa Bhavsar <bhavsarparshwa@gmail.com>
2022-12-19 23:57:03 +0530

    c

--- C Programming/Assignment/Module 3 (Fundamental of Programming)/3.3 (File Handling and Debugging)/Que 1.c
+++ C Programming/Assignment/Module 3 (Fundamental of Programming)/3.3 (File Handling and Debugging)/Que 1.c
@@ -1,2 +1,48 @@
 // Que 1 : Write a program to find out the max number from given array using function.
+
 #include  <stdio.h>
+//
+//void max()
+//{
+//	int arr[6];
+//	int i;
+//	
+//	for(i=0;i<6;i++)
+//	{
+//		if(arr[0]>arr[i])
+//		{
+//			printf("\n%d is Max number from given array",arr[0]);
+//		}
+//		
+//	}
+//}
+
+void maxx()
+{
+	int i;
+	int max;
+	int arr[6];
+	for(i=0;i<6;i++)
+	{
+		if(arr[i]>max)
+		{
+			max = arr[i];
+		}
+	}
+	printf("max = %d",max);
+}
+void main()
+{
+	int arr[6];
+	int i,length,max;
+	
+	max = arr[0];
+		
+	for(i=0;i<6;i++)
+	{
+		printf("\nEnter element [%d] : ",i);
+		scanf("%d",&arr[i]);
+	}
+
+	maxx();	
+}
--- C Programming/Assignment/Module 3 (Fundamental of Programming)/3.3 (File Handling and Debugging)/Que 7.c
+++ C Programming/Assignment/Module 3 (Fundamental of Programming)/3.3 (File Handling and Debugging)/Que 7.c
@@ -1,1 +1,20 @@
-//Que 7 : WAP Find out length of string without using inbuilt function.
+//Que 7 : Write a program to Find out length of string without using inbuilt function.
+
+#include <stdio.h>
+
+void main()
+{
+	char str[30];
+	int i,len=0;													
+	
+	printf("\nEnter a string of your choice : ");
+	gets(str);
+	
+	for(i=0;str[i]!='\0';i++)													//using len variable to obtain the length of the string
+	{																			// we are using the for loop till the last character of the string 
+		len++;																	//here count of 'i' will show us the length of the string as the loop runs
+	}
+	
+	printf("\n\nYou entered the string [%s] ",str);
+	printf("And it\'s the length is %d",len);
+}
