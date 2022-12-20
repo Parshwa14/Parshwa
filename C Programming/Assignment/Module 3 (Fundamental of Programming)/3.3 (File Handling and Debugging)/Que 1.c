0c9b1719cd750e14aa342a316b758e579f237738
Parshwa Bhavsar <bhavsarparshwa@gmail.com>
2022-12-20 13:20:22 +0530

    c

--- C Programming/Assignment/Module 3 (Fundamental of Programming)/3.3 (File Handling and Debugging)/Que 1.c
+++ C Programming/Assignment/Module 3 (Fundamental of Programming)/3.3 (File Handling and Debugging)/Que 1.c
@@ -1,48 +1,38 @@
 // Que 1 : Write a program to find out the max number from given array using function.
 
 #include  <stdio.h>
-//
-//void max()
-//{
-//	int arr[6];
-//	int i;
-//	
-//	for(i=0;i<6;i++)
-//	{
-//		if(arr[0]>arr[i])
-//		{
-//			printf("\n%d is Max number from given array",arr[0]);
-//		}
-//		
-//	}
-//}
 
-void maxx()
+void max(int arr1[],int length)												//taking two parameters(arguments) to take value from the main function
 {
 	int i;
-	int max;
-	int arr[6];
-	for(i=0;i<6;i++)
+	int max = arr1[0];														//declaring the max variable to any of array element for the ease of comparison
+	
+	for(i=0;i<7;i++)
 	{
-		if(arr[i]>max)
-		{
-			max = arr[i];
+		printf("%d \t",arr1[i]);											//printing the elements of array	
+		
+		if(arr1[i]>max)														
+		{																	//to find the max number from the array by comparing each element with the first 
+			max = arr1[i];													//element of the array
 		}
 	}
-	printf("max = %d",max);
+	printf("\n\nThe Max Number in the Array is : %d",max);
 }
+
+
 void main()
 {
-	int arr[6];
-	int i,length,max;
 	
-	max = arr[0];
-		
-	for(i=0;i<6;i++)
-	{
-		printf("\nEnter element [%d] : ",i);
-		scanf("%d",&arr[i]);
-	}
+	int arr1[10] ;															//we are taking here array of 10 numbers
+	int i;														
+	int length = sizeof(arr1)/sizeof(arr1[0]);								//for finding length we are using total size of array/size of single element of array
 
-	maxx();	
+	for(i=0;i<length;i++)
+	{
+		printf("\nEnter element %d : ",i);									//taking inputs of array from the user
+		scanf("%d",&arr1[i]);
+	}
+	printf("\n\n\n");
+	max(arr1,length);														//calling the function
 }
+
