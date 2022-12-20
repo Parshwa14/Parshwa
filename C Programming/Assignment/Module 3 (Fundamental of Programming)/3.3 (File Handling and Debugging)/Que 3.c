366b45f08175bae02b55d78ce0f8b48f8d824c26
Parshwa Bhavsar <bhavsarparshwa@gmail.com>
2022-12-20 14:05:21 +0530

    c

--- C Programming/Assignment/Module 3 (Fundamental of Programming)/3.3 (File Handling and Debugging)/Que 3.c
+++ C Programming/Assignment/Module 3 (Fundamental of Programming)/3.3 (File Handling and Debugging)/Que 3.c
@@ -2,8 +2,25 @@
 
 #include <stdio.h>
 
-void main()
+
+void reverseSentence() 
 {
-	char s[20];
-	
+    char c;								
+    scanf("%c", &c);													//accepting the inpuot from user		
+    if (c != '\n') 														//condition for further opertaion if c is char then the code will work 
+	{
+        reverseSentence();												//calling the function in itself (recursion)
+        printf("%c", c);												//printing the output one by one letter as we have used the char data type 
+    }																	//it will work with only one char at the time
 }
+
+
+void main() 
+{
+   	printf("Enter a sentence: ");										//showing msg for user to input the string	
+    reverseSentence();													//calling the function
+    
+}
+
+
+	
