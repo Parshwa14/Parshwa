bc5bc1d041c6126d328c51207c953fd83eaf8194
Parshwa Bhavsar <bhavsarparshwa@gmail.com>
2022-12-23 02:21:15 +0530

    c

--- C Programming/Assignment/Module 3 (Fundamental of Programming)/3.3 (File Handling and Debugging)/Que 3.c
+++ C Programming/Assignment/Module 3 (Fundamental of Programming)/3.3 (File Handling and Debugging)/Que 3.c
@@ -1,15 +1,16 @@
 //Que 3 : Write a program to find reverse of string using recursion.
 
+
 #include <stdio.h>
 
 
-void reverseSentence() 
+void revstr() 
 {
     char c;								
-    scanf("%c", &c);													//accepting the inpuot from user		
-    if (c != '\n') 														//condition for further opertaion if c is char then the code will work 
+    scanf("%c", &c);													//accepting the input from user		
+    if (c != '\n') 														//condition for further opertaion if c is char and not a new line then it will proceed further 
 	{
-        reverseSentence();												//calling the function in itself (recursion)
+        revstr();														//calling the function in itself (recursion)
         printf("%c", c);												//printing the output one by one letter as we have used the char data type 
     }																	//it will work with only one char at the time
 }
@@ -18,7 +19,7 @@ void reverseSentence()
 void main() 
 {
    	printf("Enter a sentence: ");										//showing msg for user to input the string	
-    reverseSentence();													//calling the function
+    revstr();															//calling the function
     
 }
 
