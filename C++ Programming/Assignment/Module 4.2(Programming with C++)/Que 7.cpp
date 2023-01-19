e9fb874b97d1294f895436540f3aa0efd7eb1cfa
Parshwa Bhavsar <bhavsarparshwa@gmail.com>
2023-01-20 01:32:47 +0530

    C++

--- C++ Programming/Assignment/Module 4.2(Programming with C++)/Que 7.cpp
+++ C++ Programming/Assignment/Module 4.2(Programming with C++)/Que 7.cpp
@@ -5,9 +5,51 @@
 
 
 #include<iostream>
-using namepsace std;
+using namespace std;
+
+class Student
+{
+	public:												//Parent class
+		int Rollno;
+};
+
+class Test:public Student
+{														//child class (Parent class for Result)
+	public:
+		int Math,Sci;
+		
+};
+
+class Result:public Test
+{														//child class
+	public:
+		int Total;
+		
+		
+		void set()												//method for getting input from user
+		{
+			cout<<"\nEnter Roll Number :";
+			cin>>Rollno;
+			cout<<"\nEnter Maths Marks :";
+			cin>>Math;
+			cout<<"\nEnter Science Marks :";
+			cin>>Sci;
+		}
+		
+		void show()
+		{
+			cout<<"\nRoll no : "<<Rollno;						//method for showing the inputs and the data
+			cout<<"\nMath Marks : "<<Math;
+			cout<<"\nScience Marks : "<<Sci;
+			cout<<"\nTotal Marks : "<<(Math+Sci);
+		}
+		
+};
 
 int main()
 {
+	Result Res;													//object creation of the derived class
+	Res.set();
+	Res.show();
 	return 0;
 }
