0ae2b4301dd6117adb5d18ae0b43496a80e2d3c6
Parshwa Bhavsar <bhavsarparshwa@gmail.com>
2023-01-31 02:46:58 +0530

    C++

--- C++ Programming/Assignment/Module 4.2(Programming with C++)/Que 10.cpp
+++ C++ Programming/Assignment/Module 4.2(Programming with C++)/Que 10.cpp
@@ -1,15 +1,48 @@
 //Que 10. Write a program to concatenate the two strings using Operator Overloading.
-
 #include<iostream>
+#include<string.h>
 using namespace std;
 
-class ConStr
+class String
 {
-	public:
-		
+        public:
+            char str[50];
+        
+            void input()
+            {													//taking string input from user
+                cout<<"\n Enter String : ";
+                cin.getline(str,50);
+	        }
+            void display()
+			{
+                cout<< str;
+            }
+            String operator+(String x)  						//Concatenating String using operator overloading
+            {
+				String s;
+                strcat(str,x.str);
+                strcpy(s.str,str);
+			    return s;
+            }
 };
-
 int main()
 {
-return 0;                        
+    String str1, str2, str3;
+
+    str1.input();
+    str2.input();
+
+    cout<<"\n ----------------------------------------------";
+    cout<<"\n\n First String is           :  ";
+    str1.display();   														//Displaying First String
+
+    cout<<"\n\n Second String is          :  ";
+    str2.display();  														//Displaying Second String
+
+    cout<<"\n ----------------------------------------------";
+    str3=str1+str2;        													//String is concatenated. Overloaded '+' operator
+    cout<<"\n\n Concatenated String is    :  ";
+    str3.display();
+
+    return 0;
 }
